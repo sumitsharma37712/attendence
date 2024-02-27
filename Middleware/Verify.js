63e47064b4c914e4ca96af1b778d6943860a11f5
sumit sharma <sumitsharma37712>
2024-02-27 13:13:26 +0530

    Update Verify.js

--- Middleware/Verify.js
+++ Middleware/Verify.js
@@ -1,22 +1,23 @@
 const jwt = require ('jsonwebtoken');
+const secretKey='Thisithetokenfoadminregiration';
 
 const VerifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   const token = authHeader.split(" ")[1];
   if (authHeader) {
-    jwt.verify (token, process.env.JWT, (err, user) => {
-      if (err) res.status (403).json ('Token is not valid');
+    jwt.verify (token, secretKey, (err, user) => {
+      if (err) res.status (403).json ({error:'Token is not valid'});
       req.user = user;
       next ();
     });
   } else {
-    return res.status (500).json ('You are not authenticate');
+    return res.status (500).json ({error:'You are not authenticate'});
   }
 };
 
 const VerifyTokenAndAuthorization = (req, res, next) => {
   VerifyToken (req, res, () => {
-    if (req.user.id === req.params.id || req.user.isAdmin) {
+    if (req.user.id === req.params.id || req.user.type) {
       next ();
     } else {
       return res.status (403).json ('You are not allowed to do that!');
@@ -26,10 +27,11 @@ const VerifyTokenAndAuthorization = (req, res, next) => {
 
 const VerifyTokenAndAdmin = (req, res, next) => {
   VerifyToken (req, res, () => {
-    if (req.user.isAdmin) {
+    console.log(req.user)
+    if (req.user.type=='admin') {
       next ();
     } else {
-      return res.status (403).json ('You are not allowed to do that!');
+      return res.status (403).json ({error:'You are not allowed to do that!'});
     }
   });
 };
