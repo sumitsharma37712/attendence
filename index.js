0ec9b4442428cffc020a5de7edea7705f1c6c051
sumit sharma <sumitsharma37712>
2024-02-27 13:13:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const con = require('./databases/config');
-const router = require('./Routers/router');
-const app = new express();
+const express = require ('express');
+const con = require ('./databases/config');
+const router = require ('./Routers/router');
+const app = new express ();
 const PORT = process.env.PORT || 4000;
-const cors = require('cors');
+const cors = require ('cors');
 // const connectDB = require('./database/config');
 
 // connectDB()
 // Order of middleware matters
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ["GET", "POST","PUT","DELETE"],
-    credentials: true
-}));
+app.use (
+  cors ({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 
-app.use(express.json());
-app.use(router);
+app.use (express.json ());
+app.use (router);
 
-app.listen(PORT, () => {
-    console.log(`port start will be ${PORT}`);
+app.listen (PORT, () => {
+  console.log (`port start will be ${PORT}`);
 });
